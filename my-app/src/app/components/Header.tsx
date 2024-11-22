aad4da95eb2c8b0d3583b2c5eb1aa325372d9967
mabagit config --global user.name maba <gyj0149@gmail.com>
2024-11-23 07:56:09 +0900

    build

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -1,19 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-white shadow-md w-full">
-      <div className="w-full flex items-center justify-between py-4 px-6">
-        <a href="/" className="text-xl font-bold text-blue-500">
-          로고
-        </a>
-        <nav>
-          <a href="/about" className="text-gray-700 hover:text-blue-500 mx-4">
-            소개
-          </a>
-          <a href="/contact" className="text-gray-700 hover:text-blue-500 mx-4">
-            연락처
-          </a>
-        </nav>
-      </div>
+      <div className="w-full flex items-center justify-between py-4 px-6"></div>
     </header>
   );
 }
